21ee6b0195d015543cc7b8a64d4ef130425873fc
Alejandromarchesini12 <alejandro.marchesini12@gmail.com>
2025-11-25 15:35:39 -0800

    Finish visualize.py

--- visualize.py
+++ visualize.py
@@ -12,7 +12,31 @@ def print_gantt_chart (gantt_data: list[tuple[str,int]]):
     merged = []
     current_thread, start_time = gantt_data[0]
 
-    # fiish here
+    # Build merged blocks like: T1, start=0, end = 3
+    for i in range(1, len(gantt_data)): 
+        thread, time = gantt_data[i]
+
+        if thread != current_thread: 
+            # close previous block at this time
+            merged.append((current_thread, start_time, time))
+            current_thread = thread
+            start_time = time
+
+    # add the final block
+    last_time = gantt_data[-1][1] + 1
+    merged.append((current_thread, start_time, last_time))
+
+    # print the merge blocks
+    for thread, start, end in merged: 
+        duration = end - start
+        label = f"{thread}"
+
+        if thread == "IDLE": # if nothing is running in the CPU 
+            print(f"[{start:02d}-{end:02d}] CPU IDLE ({duration} unit)")
+        else: 
+            print(f"[{start:02d}-{end:02d}] {label:<5} ran ({duration} units)")
+
+    print("----------------------------------------\n")
 
 def print_metrics_table (metrics: dict, threads: list):
     '''
@@ -20,6 +44,13 @@ def print_metrics_table (metrics: dict, threads: list):
     Metrics come from mentrics.py'''
 
     print("\n----------- METRICS SUMMARY ----------")
+    print(f"{'Thread':<08}{'Arrive':<08}{'Burst':<08}{'Finish':<08}{'Waitign':<10}{'Turnaround':<12}") 
+    
+    for th in threads: 
+        print(f"{th.thread_id:<08}{th.arrival:<08}{th.burst:<08}{th.completion_time:<08}{th.waiting_time:<10}{th.turnaround_time:<12}")
 
-    # finish here 
-    return 
+    print("\nAverages:")
+    print(f"Average Waiting Time    : {metrics['average_waiting_time']: .2f}")
+    print(f"Average Turnaround Time : {metrics['average_turnaround_time']: .2f}")
+    print(f"CPU Utilization         : {metrics['cpu_utilization']: .2f}%")
+    print("----------------------------------------\n")
